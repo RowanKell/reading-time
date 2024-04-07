78fe0d1c320a8578fae075abac043b5577c5677c
Rowan Kelleher <rowan.kelleher@duke.edu>
2024-04-07 14:40:07 -0400

    initial commit

---
+++ scripts/content.js
@@ -0,0 +1,20 @@
+console.log("working")
+
+//const mainContent = document.querySelector("header");
+//const mainContent = document.querySelector("#mount_0_0_xx > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div > div.x78zum5.xdt5ytf.x1t2pt76.x1n2onr6.x1ja2u2z.x10cihs4 > div.x9f619.xvbhtw8.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1uhb9sk.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1qughib > div.x1gryazu.xh8yej3.x10o80wk.x14k21rp.x17snn68.x6osk4m.x1porb0y > section > div:nth-child(2) > nav > div > header")
+
+const mainContent document.querySelector("#mount_0_0_8m > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div > div.x78zum5.xdt5ytf.x1t2pt76.x1n2onr6.x1ja2u2z.x10cihs4 > div.x9f619.xvbhtw8.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1uhb9sk.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1qughib > div.x1gryazu.xh8yej3.x10o80wk.x14k21rp.x17snn68.x6osk4m.x1porb0y > section > div:nth-child(2) > nav > div > header > div > h1 > div > div > div > div.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1n2onr6.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.x1q0g3np.xqjyukv.x1pha0wt.x1oa3qoh.x1nhvcw1 > div.x1qjc9v5.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x78zum5.xdt5ytf.x2lah0s.xr9ek0c.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x11njtxf > span")
+
+
+if (mainContent) {
+    //const readingTime = Math.round(wordCount / 200);
+    const badge = document.createElement("h1");
+    //Use the same styling as the publish information in an article's header
+    badge.classList.add("color-secondary-text", "type--caption");
+    badge.textContent = `STOP, YOU SHOULD BE WORKING!`;
+
+    // Insert the badge after the first heading or at the beginning of the main content
+    //const firstHeading = mainContent.querySelector("h1") || mainContent.querySelector("h2");
+    mainContent.insertAdjacentElement("afterbegin",badge)
+    //(firstHeading || mainContent).insertAdjacentElement("afterbegin", badge);
+}
